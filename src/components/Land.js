a8963af112bd9944792696c4814975aafad55cfc
Shreyas Balaji Nagaraja <sh529060@dal.ca>
2024-01-01 20:15:50 -0400

    Basic Idea Implementation

--- src/components/Land.js
+++ src/components/Land.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Mountain from '../Assets/Mountain.png'
+import Moon from '../Assets/Moon.png'
+import Sun from '../Assets/Sun.png'
+import { useState } from 'react'
 export default function Land() {
+  const [bg,changeBg]=useState(true);
+  const [cel, changeCel]=useState(true);
+  const [bri, changeBri]=useState(true);
+  const handleChange=()=>{
+        changeBg(!bg)
+        changeCel(!cel)
+        changeBri(!bri)
+  }
   return (
-    <div className='text-red-950 w-screen h-screen grid grid-cols-2'>
-    <div className=' bg-black w-full h-screen flex justify-center text-center items-center object-cover'>
-        Name 1</div>
-    <div className='bg-slate-500'>Name 2</div>
+    <div className={bg?'bg-sky-400':'bg-slate-700'}>
+    <img src={cel?Sun:Moon} className=' w-24 h-24 z-10 fixed hover:w-48 hover:h-48 mt-10 right-32 transition-all' onClick={handleChange}></img>
+    <img src={Mountain} className='bg-cover w-screen h-screen' style={{ filter: bri?'brightness(90%)':'brightness(35%)' }}alt="Mountain"/>
     </div>
   )
 }
